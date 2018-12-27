6191bc129be58e4d6c071e85d548929cd7d8d127
Joris Ros <j.ros@youwe.nl>
2018-12-27 11:56:06 +0100

    Update install sh file change newline and store ini file correct

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo "\nDownload docker-compose.yml ...\n"
+printf "\nDownload docker-compose.yml ...\n"
 curl https://raw.githubusercontent.com/jorisros/pimcore-docker-compose/master/docker-compose.yml --output docker-compose.yml
 
-echo "Create the .docker/php directory\n"
+printf "Create the .docker/php directory\n"
 mkdir -p .docker/php
-echo "Download php-ini-overrides.ini\n"
-curl https://raw.githubusercontent.com/jorisros/pimcore-docker-compose/master/.docker/php/php-ini-overrides.ini .docker/php/php-ini-overrides.ini
-echo "Cleanup.\n"
+printf "Download php-ini-overrides.ini\n"
+curl https://raw.githubusercontent.com/jorisros/pimcore-docker-compose/master/.docker/php/php-ini-overrides.ini --output .docker/php/php-ini-overrides.ini
+printf "Cleanup.\n"
 rm install-docker-compose.sh
